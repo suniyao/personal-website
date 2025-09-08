f60c96eb3b47e9ffbd4cb58cf6857fa0b985aae9
StephYSN <3011316681@qq.com>
2025-09-08 11:02:54 -0700

    add update page style

--- app/(pages)/update/page.tsx
+++ app/(pages)/update/page.tsx
@@ -1,6 +1,7 @@
 import { geologica } from "@/ui/fonts";
 import Image from "next/image";
 import { getSortedPostsData } from "@/lib/posts";
+import Blog from "@/components/Blog";
 
 export default async function Update() {
   const allPostsData = await getSortedPostsData();
@@ -21,20 +22,12 @@ export default async function Update() {
           height={100}
         />
       </a>
-      <div className="text-[20px] mt-10">
-        <ul>
-          {allPostsData.map(({ id, date, title, subtitle }: { id: string; date: string; title: string; subtitle: string; }) => (
-            <li key={id}>
-              {title}
-              <br />
-              {subtitle}
-              <br />
-              {id}
-              <br />
-              {date}
-            </li>
+      <div className="">
+        <div className="flex flex-col items-center divide-y divide-gray-700">
+          {allPostsData.map(({ id, date, title, subtitle, cover }: { id: string; date: string; title: string; subtitle: string; cover: string;}) => (
+            <Blog key={id} id={id} date={date} title={title} subtitle={subtitle} cover={cover} />
           ))}
-        </ul>
+        </div>
       </div>
     </div>
   );
