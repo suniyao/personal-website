ca1bd5a13fad68eeffdd4986f6aaa3e1ae74923d
stephsnyao@gmail.com <3011316681@qq.com>
2025-12-01 12:28:20 -0800

    changed graphene details

--- app/(pages)/project/page.tsx
+++ app/(pages)/project/page.tsx
@@ -11,13 +11,7 @@ export default function Project(){
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mt-10 w-full items-stretch justify-items-center">
         <Card title="Graphene" link="https://github.com/suniyao/graphene">
           <p>
-          An {" "} 
-          <span className="inline-block">
-            <SiObsidian color="#A28AE5"/>
-          </span> Obsidian plugin powered by AI to help you construct a better second brain. 
-          </p>
-          <p>
-            Major features include note auto-summarization and tagging, improved graph view in visualizing inner correlations between files, AI explanations of difficult concepts with reference to the notes, and more. 
+            An Obsidian plugin that makes semantic graph view based on content similarity using vector embeddings, with local vector embedding supported by sentence-transformers python package and alternative OpenAI embedding model, and various options for graph appearance setting including particle animation, link thickness, etc.
           </p>
         </Card>
 
